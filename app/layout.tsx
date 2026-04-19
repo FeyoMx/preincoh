4e44bbd5a11fcc72cb8ac1c3d231e10f107d5fa3
FeyoMx <elfeyo1980@gmail.com>
2026-04-18 20:27:54 -0600

    feat: add facebook domain verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#003366" />
         <meta name="msapplication-TileColor" content="#003366" />
+        <meta name="facebook-domain-verification" content="sv159kxomavpv9g2iu2nlv64fspyzv" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
